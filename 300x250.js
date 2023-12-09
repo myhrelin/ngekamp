932412505820370cad862575ca513a03d911e903
myhrelin <77819233+myhrelin@users.noreply.github.com>
2023-12-09 20:47:50 +0700

    Add files via upload

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+        'key' : '358cf4b6b7d44f441354d709e893d789',
+        'format' : 'iframe',
+        'height' : 250,
+        'width' : 300,
+        'params' : {}
+    };
+    document.write('<scr' + 'ipt type="text/javascript" src="//www.topcreativeformat.com/358cf4b6b7d44f441354d709e893d789/invoke.js"></scr' + 'ipt>');
